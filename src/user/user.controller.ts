0f00bb79c3e930276064cabf68a350ff54923ea1
Aleksandr <alokil70@gmail.com>
2021-04-29 22:58:09 +0300

    create user get and post request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
+import { DeleteResult } from 'typeorm';
 
 @Controller('user')
 export class UserController {
@@ -12,11 +13,23 @@ export class UserController {
 		return this.userService.getAll(createUserDto);
 	}
 
+	@Get(':id')
+	getById(@Param('id', ParseIntPipe) id: number): Promise<User> {
+		return this.userService.getById(id);
+	}
+
 	@Post()
 	create(@Body() createUserDto: CreateUserDto): Promise<User> {
 		return this.userService.create(createUserDto);
 	}
 
 	@Delete(':id')
-	delete() {}
+	delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+		return this.userService.delete(id);
+	}
+
+	@Patch(':id')
+	update(@Param('id', ParseIntPipe) id: number, @Body() createUserDto: CreateUserDto): Promise<User> {
+		return this.userService.update(id, createUserDto);
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { NOT_FOUND } from '../constants/message.constants';
+import { DeleteResult } from 'typeorm';
 
 @Injectable()
 export class UserService {
@@ -17,7 +18,33 @@ export class UserService {
 		return found;
 	}
 
+	async getById(id: number): Promise<User> {
+		const found = await this.userRepository.findOne(id);
+		if (!found) {
+			throw new NotFoundException(NOT_FOUND);
+		}
+		return found;
+	}
+
 	create(createUserDto: CreateUserDto): Promise<User> {
 		return this.userRepository.createUser(createUserDto);
+	}
+
+	async delete(id: number): Promise<void> {
+		const result = await this.userRepository.delete(id);
+		if (result.affected === 0) {
+			throw new NotFoundException(NOT_FOUND);
+		}
+	}
+
+	async update(id: number, createUserDto: CreateUserDto): Promise<User> {
+		const found = await this.getById(id);
+		const { name, email, password, city } = createUserDto;
+		found.email = email;
+		found.city = city;
+		found.name = name;
+		found.password = password;
+		await found.save();
+		return found;
 	}
 }
